301b466054456788bdb081598b054d77a294eda6
saranyac22@gmail.com <saranyac22@gmail.com>
2021-07-30 19:34:55 +0000

    Phase 4 Assessment - Online Test Application

---
+++ js/Quiz.js
@@ -0,0 +1,42 @@
+var jsonData = [
+    {
+        "q" : "	Guwahati High Court is the judicature of",
+        "opt1" : "Nagaland",
+        "opt2" : "Arunachal Pradesh",
+        "opt3" : "Arunachal Pradesh",
+        "opt4" : "All of the Above",
+        "answer" : "All of the Above"
+    },
+    {
+        "q" : "Fire temple is the place of worship of which of the following religion?",
+        "opt1" : "Taoism",
+        "opt2" : "Judaism",
+        "opt3" : "Zoroastrianism (Parsi Religion)",
+        "opt4" : "Shintoism",
+        "answer" : "Zoroastrianism (Parsi Religion)"
+    },
+    {
+        "q" : "Film and TV institute of India is located at",
+        "opt1" : "Pune (Maharashtra)",
+        "opt2" : "Rajkot (Gujarat)",
+        "opt3" : "Pimpri (Maharashtra)",
+        "opt4" : "Kochi (Kerala)",
+        "answer" : "Pune (Maharashtra)"
+    },
+    {
+        "q" : "Ecology deals with",
+        "opt1" : "Birds",
+        "opt2" : "Cell formation",
+        "opt3" : "Relation between organisms and their environment",
+        "opt4" : "Tissues",
+        "answer" : "Relation between organisms and their environment"
+    },
+    {
+        "q" : "Goa Shipyard Limited (GSL) was established in",
+        "opt1" : "1958",
+        "opt2" : "1957",
+        "opt3" : "1956",
+        "opt4" : "1955",
+        "answer" : "opt2"
+    }
+];
